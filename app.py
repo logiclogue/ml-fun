a474e98f3378f8b8ccab44502ec3a25ac3163eeb
NavyaZaveri <navyajayesh+1@gmail.com>
2017-11-05 15:50:21 +0000

    added flask app

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask,url_for,render_template,request,redirect
+
+app = Flask(__name__)
+
+@app.route("/",methods=["GET"])
+def index():
+	return render_template("index.html")
+
+@app.route("/result",methods=["POST"])
+def result():
+	print(request.get_data())
+	return "works!" 
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
